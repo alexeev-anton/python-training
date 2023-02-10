12b4116a09e2e3d110ff853a38ac17a7634b3173
aalakseev <aalakseev@diasoft.ru>
2023-02-10 16:08:49 +0300

    Созданы вспомогательные методы

--- test_add_group.py
+++ test_add_group.py
@@ -13,14 +13,21 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
-        wd.find_element(By.ID, "LoginForm").click()
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
+
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "group page").click()
+
+    def create_group(self, wd):
         wd.find_element(By.NAME, "new").click()
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
@@ -32,8 +39,19 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys("test")
         wd.find_element(By.NAME, "submit").click()
-        wd.find_element(By.LINK_TEXT, "group page").click()
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element(By.LINK_TEXT, "groups").click()
+
+    def login(self, wd):
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
