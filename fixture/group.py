fa54cbea78ee38be9e102428db544365ef480254
Anton Alekseev <aalakseev@diasoft.ru>
2023-02-17 15:54:43 +0300

    Добавлен conftest.py, добавлен тест для удаления групп

--- fixture/group.py
+++ fixture/group.py
@@ -25,6 +25,13 @@ class GroupHelper:
         wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element(By.NAME, "selected[]").click()
+        wd.find_element(By.NAME, "delete").click()
+        self.return_to_groups_page()
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "group page").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,16 +4,9 @@ from model.contact import Contact
 from fixture.application import Application
 
 
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-
 def test_add_contact(app):
     app.session.login("admin", "secret")
     app.contact.create_contact(Contact(firstname="Jack", lastname="Daniels", nickname="JD", company="Whiskey",
-                               address="Scotland", home="999-999-99-99", work="777-777-77-77",
-                               email="jd@test.com"))
+                                       address="Scotland", home="999-999-99-99", work="777-777-77-77",
+                                       email="jd@test.com"))
     app.session.logout()
