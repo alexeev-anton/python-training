96944f380bc8aa97ef58b90e07ccd600b53c052b
Anton Alekseev <aalakseev@diasoft.ru>
2023-02-19 12:01:20 +0300

    Добавлен тест для модификации группы

--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import *
+from model.group import Group
 
 
 class GroupHelper:
@@ -13,6 +14,26 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element(By.NAME, "new").click()
+        self.fill_group_data(wd, group, "submit")
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element(By.NAME, "selected[]").click()
+        wd.find_element(By.NAME, "delete").click()
+        self.return_to_groups_page()
+
+    def edit_group_create_if_missing(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups_count = len(wd.find_elements(By.NAME, "selected[]"))
+        if groups_count == 0:
+            self.create(Group(name="test", header="test", footer="test"))
+        wd.find_element(By.NAME, "selected[]").click()
+        wd.find_element(By.NAME, "edit").click()
+        self.fill_group_data(wd, group, "update")
+
+    def fill_group_data(self, wd, group, flag):
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
         wd.find_element(By.NAME, "group_name").send_keys(group.name)
@@ -22,22 +43,11 @@ class GroupHelper:
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        wd.find_element(By.NAME, "submit").click()
+        if flag == "submit":
+            wd.find_element(By.NAME, "submit").click()
+        if flag == "update":
+            wd.find_element(By.NAME, "update").click()
         self.return_to_groups_page()
-
-    def delete_first_group(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        wd.find_element(By.NAME, "selected[]").click()
-        wd.find_element(By.NAME, "delete").click()
-        self.return_to_groups_page()
-
-    def edit_group_create_if_missing(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups_count = wd.find_elements(By.NAME, "selected[]").count()
-
-
 
     def return_to_groups_page(self):
         wd = self.app.wd
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -6,5 +6,5 @@ from fixture.application import Application
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_group_create_if_missing()
+    app.group.edit_group_create_if_missing(Group(name="test_updated", header="test_updated", footer="test_updated"))
     app.session.logout()
