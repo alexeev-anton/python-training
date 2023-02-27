3fb6a09e82145c78c874765be31b3e46901bdfb1
Anton Alekseev <aalakseev@diasoft.ru>
2023-02-27 10:35:05 +0300

    Добавлена проверка текущего состояния сессии

--- fixture/session.py
+++ fixture/session.py
@@ -17,3 +17,25 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element(By.LINK_TEXT, "Logout").click()
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[1]/form[1]/b[1]").text == "(" + username + ")"
