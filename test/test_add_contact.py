020f22266b81e56c7f23193f012f096c5885c25a
Anton Alekseev <aalakseev@diasoft.ru>
2023-02-28 09:37:06 +0300

    Оптимизированы условия

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,9 +3,7 @@ from model.group import Group
 
 
 def test_add_contact(app):
-    if app.group.groups_count() > 0 and app.group.check_group_name("group_name"):
-        pass
-    else:
+    if not (app.group.groups_count() > 0 and app.group.check_group_name("group_name")):
         app.group.create(Group(name="group_name", header="group_header", footer="group_footer"))
     app.contact.create_contact(Contact(firstname="Jack", lastname="Daniels", nickname="JD", company="Whiskey",
                                        address="Scotland", home="999-999-99-99", work="777-777-77-77",
